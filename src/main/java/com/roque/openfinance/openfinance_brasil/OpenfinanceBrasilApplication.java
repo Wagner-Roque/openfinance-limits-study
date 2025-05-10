9895f504dd5dc41db33b872c3b5551e3db7b3876
Roque <roquewagnertadeu@gmail.com>
2025-05-10 20:21:14 -0300

    first commit

---
+++ src/main/java/com/roque/openfinance/openfinance_brasil/OpenfinanceBrasilApplication.java
@@ -0,0 +1,13 @@
+package com.roque.openfinance.openfinance_brasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfinanceBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfinanceBrasilApplication.class, args);
+	}
+
+}
